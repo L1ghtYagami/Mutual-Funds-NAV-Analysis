0554b455f5926a42f51b992f9c1b8460486e7de8
L1ghtYagami <aneeshp1994@gmail.com>
2020-12-30 14:17:17 +0530

    Added specific mutual fund names

--- Utils/config.R
+++ Utils/config.R
@@ -1,2 +1,17 @@
 # This file stores all the variales that are required in multiple files
 mutual_fund_names <- c("Mirae", "SBI", "Axis")
+
+# Specific Mutual funds (optional)
+# Funds not yet invested into - c("Axis Multicap Fund")
+mut_mir <- c("Mirae Asset Focused Fund Direct Plan Growth",
+             "Mirae Asset Large Cap Fund - Direct Plan - Growth")
+mut_sbi <- c("SBI Small Cap Fund - Direct Plan - Growth")
+mut_axi <- c("Axis Small Cap Fund - Direct Plan - Growth",
+             "Axis Midcap Fund - Direct Plan - Growth",
+             "Axis Bluechip Fund - Direct Plan - Growth",
+             "Axis Multicap Fund - Direct Plan - Growth")
+
+mut_all <- c(mut_mir, mut_sbi, mut_axi)
+mut_all_list <- list(mut_mir,  mut_sbi, mut_axi)
+names(mut_all_list) <- mutual_fund_names
+
