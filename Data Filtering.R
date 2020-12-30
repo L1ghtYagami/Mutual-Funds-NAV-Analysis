7cca413d14bf96c5b2db88580624719aa3b03dce
L1ghtYagami <aneeshp1994@gmail.com>
2020-12-30 14:23:21 +0530

    Removed unncessary lines of code

--- Data Filtering.R
+++ Data Filtering.R
@@ -1,24 +1,16 @@
-# Load libraries
-
-
 # Source files from which to import some common names
 source("./Utils/config.R")
 
-# Specific Mutual funds (optional)
-# Funds not yet invested into - c("Axis Multicap Fund")
-mut_mir <- c("Mirae Asset Focused Fund Direct Plan Growth",
-             "Mirae Asset Large Cap Fund - Direct Plan - Growth")
-mut_sbi <- c("SBI Small Cap Fund - Direct Plan - Growth")
-mut_axi <- c("Axis Midcap Fund - Direct Plan - Growth",
-             "Axis Bluechip Fund - Direct Plan - Growth",
-             "Axis Multicap Fund - Direct Plan - Growth")
-
-mut_all <- c(mut_mir, mut_sbi, mut_axi)
-
-for (mutual_fund in mutual_fund_names) {
-    data <- read.csv(paste("./Data/Pre-processed/", mutual_fund, '.csv',
-                           sep = ''))
-    data <- data[data$Scheme.Name %in% mut_all,]
-    write.csv(data, paste("./Data/Filtered Data/", mutual_fund, '.csv',
-                          sep = ''))
+# Define a function that loads and filters the data
+# funds_filter is the vector of all mutual funds whose data you want
+filter_data <- function(funds_filter) {
+    for (mutual_fund in mutual_fund_names) {
+        data <- read.csv(paste("./Data/Pre-processed/", mutual_fund, '.csv',
+                               sep = ''))
+        data <- data[data$Scheme.Name %in% funds_filter,]
+        write.csv(data, paste("./Data/Filtered Data/", mutual_fund, '.csv',
+                              sep = ''), row.names = FALSE)
+    }
 }
+
+filter_data(funds_filter = mut_all)
